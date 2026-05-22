c45ecdd912461fa623ac961ae2d99743624818d4
lelo0002 <backupce0819@gmail.com>
2026-05-21 19:19:06 -0700

    Update nettuwai.lua

--- nettuwai.lua
+++ nettuwai.lua
@@ -247,7 +247,7 @@ local Library = {
 		RiskColor = Color3.fromRGB(255, 50, 50),
 
 		Black = Color3.new(0, 0, 0),
-    Font = Enum.Font.FredokaOne,
+    Font = Enum.Font.GothamSemiBold,
 
     -- frames --
     OpenedFrames = {};
